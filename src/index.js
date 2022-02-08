da28373bcd1530c8a4706157243c7f9eb8c7310f
nimit2801 <mnimitsavant@outlook.com>
2022-02-08 21:25:02 +0530

    Update command for .new ✅

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import {Client, CommandInteractionOptionResolver, Intents } from 'discord.js';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
+import fun from 'fun-responses'
+
 dotenv.config();
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
@@ -8,7 +10,6 @@ client.once('ready', async()=> {
     console.log('Ready');
 });
 
-import fun from 'fun-responses'
 
 async function restLoveGifs(){
     let random = Math.floor(Math.random() * 51);
@@ -19,9 +20,7 @@ async function restLoveGifs(){
 client.on('messageCreate', async(message)=> {
     if(!message.author.bot){
         if(message.content.startsWith('.new')){
-            if(message.content == '.new')
-                await message.reply(await fun.pickup())
-            else if(message.content.startsWith('.new topic'))
+            if(message.content.startsWith('.new topic'))
                 await message.reply(await fun.topic())
             else if(message.content.startsWith('.new roast'))
                 await message.reply(await fun.roast())
@@ -29,6 +28,8 @@ client.on('messageCreate', async(message)=> {
                 await message.reply(await fun.toast())
             else if(message.content.startsWith('.new joke'))
                 await message.reply(await fun.joke())
+            else if(message.content.startsWith('.new'))
+                await message.reply(await fun.pickup())
         }
         if(message.content.startsWith('.gif')){
             message.reply(await restLoveGifs(), "wait for a few")
